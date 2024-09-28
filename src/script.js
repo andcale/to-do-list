e023b9fc7b0ea91c105f80774eab42e2b6f2eb15
andcale <cale06.andrade@gmail.com>
2024-09-27 23:09:49 -0300

    criaçao dos estilos

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
-const input = document.getElementById('input')
-const ul = document.getElementById('items-list')
+// Pegando o texto
+const input = document.getElementById('input');              
 
+// Pegando a ul
+const ul = document.getElementById('items-list');
+
+// Função para adicionar o texto
 function adicionar(){
     let tarefa = input.value;
     if(input.value != ''){
@@ -9,8 +13,16 @@ function adicionar(){
         li.textContent = tarefa;
         input.value = '';
     }
-    dele
+    
 }
-
+// Pegando o botão e adicionando o evento de click
 let botao = document.getElementById('button')
 botao.addEventListener('click',adicionar)
+
+//Vericando se a tecla enter foi apertada para adicionar o texto
+function enter(event){
+    if(event.key === 'Enter'){
+        adicionar();
+    }
+}
+input.addEventListener('keyup', enter)
